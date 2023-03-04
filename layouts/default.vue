17618896c58499ba5b778e139592a72853ba622a
kaho <1316156936@qq.com>
2023-03-05 00:11:15 +0800

    feat: 开发用户平台前端就诊人管理功能

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,32 @@
 <template>
-  <div class="app-container">
+  <div class="app-container" style="height: 100vh">
     <div id="main">
       <!-- 公共头 -->
-      <myheader/>
+      <myheader />
       <div class="main-container">
-        <el-scrollbar class='page-component__scroll'>
-        <!-- 内容区域 -->
-        <nuxt/>
+        <el-scrollbar class="page-component__scroll">
+          <!-- 内容区域 -->
+          <nuxt />
         </el-scrollbar>
       </div>
-        <!-- 公共底 -->
-      <myfooter/>
+      <!-- 公共底 -->
+      <myfooter />
     </div>
   </div>
 </template>
 <script>
-import '~/assets/css/app.css'
-import '~/assets/css/chunk.css'
-import '~/assets/css/iconfont.css'
-import '~/assets/css/main.css'
+import "~/assets/css/app.css";
+import "~/assets/css/chunk.css";
+import "~/assets/css/iconfont.css";
+import "~/assets/css/main.css";
 
-import myheader from './myheader'
-import myfooter from './myfooter'
+import myheader from "./myheader";
+import myfooter from "./myfooter";
 
 export default {
   components: {
-    myheader, myfooter
-  }
-}
+    myheader,
+    myfooter,
+  },
+};
 </script>
